0159fa58f9a38f14747c9bca504d0577a553a62c
Rafael Pimenta <jrafapimenta@gmail.com>
2023-07-12 18:52:20 +0100

    delete user backend

--- webapp/price_minder/routes/backend/user_management/routes.py
+++ webapp/price_minder/routes/backend/user_management/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, url_for, redirect, flash
+from flask import Blueprint, render_template, url_for, redirect, flash, request
 from flask_login import current_user, login_required
 
 from price_minder import app, db
@@ -19,6 +19,8 @@ def check_permission():
 
 
 ##################### * Users Management * #####################
+
+# Users list
 @admin_users.route('/')
 @login_required
 def users():
@@ -31,8 +33,8 @@ def users():
     users = User.query.all()
     return render_template('./backend/user-management/users_list.html', title = title, users = users)
 
-
-@admin_users.route('/promote/<int:id>', methods = ['POST'])
+# Promoto user
+@admin_users.route('/promote_user/<int:id>', methods = ['POST'])
 @login_required
 def promote(id):
     if not check_permission():
@@ -47,3 +49,22 @@ def promote(id):
         db.session.commit()
         flash(f'{user.username} was promoted to level: 2', "success")
     return redirect(url_for('admin_users.users'))
+
+# Delete user
+@admin_users.route('/delete_user/<int:id>', methods = ['POST'])
+@login_required
+def delete(id):
+    if not check_permission():
+        flash("You don't have permissions to view this area", "danger")
+        return redirect(url_for('pages.index_pages'))
+    
+    user = User.query.filter_by(id = id).first()
+    if not user:
+        flash("Couldn't find the user you were looking for", "danger")
+    
+    # Delte user from database
+    db.session.delete(user)
+    db.session.commit()
+
+    flash(f"{user.username} deleted from database", "success")
+    return redirect(url_for('admin_user.users'))
