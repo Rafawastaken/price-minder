3ce66108257b4142ab01a022a2cbe26786e85d69
Rafael Pimenta <jrafapimenta@gmail.com>
2023-07-12 17:14:12 +0100

    quick permission acceptance to admin panel

--- webapp/price_minder/routes/backend/admin/routes.py
+++ webapp/price_minder/routes/backend/admin/routes.py
@@ -1,9 +1,18 @@
-from flask import Blueprint, render_template, flash
+from flask import Blueprint, render_template, flash, redirect, url_for
+from flask_login import login_required, current_user
 
 
 
 ##################### * Blueprint * #####################
 admin = Blueprint('admin', __name__)
+
+
+##################### * Helpers * #####################
+
+# Function to check if user has permission to access admin panel
+def check_permission():
+    if current_user.level >= 2: return True
+    return False
 
 
 ##################### * Main Admin * #####################
@@ -12,6 +21,10 @@ admin = Blueprint('admin', __name__)
 @admin.route('/')
 def admin_index():
     title = "Price Minder - Admin Panel"
+    if not check_permission():
+        flash("You don't have permissions to view this area", "danger")
+        return redirect(url_for('pages.index_pages'))
+    
     return render_template('./backend/admin/index.html', title = title)
 
 
--- webapp/price_minder/routes/backend/user_management/routes.py
+++ webapp/price_minder/routes/backend/user_management/routes.py
@@ -1,14 +1,49 @@
-from flask import Blueprint, render_template, url_for, redirect
+from flask import Blueprint, render_template, url_for, redirect, flash
+from flask_login import current_user, login_required
+
 from price_minder import app, db
+
+
 from ...frontend.users.models import User
 
 ##################### * Blueprint * #####################
 admin_users = Blueprint("admin_users", __name__)
 
 
+##################### * Helpers * #####################
+
+# Function to check if user has permission to access admin panel
+def check_permission():
+    if current_user.level >= 2: return True
+    return False
+
+
 ##################### * Users Management * #####################
 @admin_users.route('/')
+@login_required
 def users():
     title = "Users Registered"
+
+    if not check_permission():
+        flash("You don't have permissions to view this area", "danger")
+        return redirect(url_for('pages.index_pages'))
+    
     users = User.query.all()
     return render_template('./backend/user-management/users_list.html', title = title, users = users)
+
+
+@admin_users.route('/promote/<int:id>', methods = ['POST'])
+@login_required
+def promote(id):
+    if not check_permission():
+        flash("You don't have permissions to view this area", "danger")
+        return redirect(url_for('pages.index_pages'))
+
+    user = User.query.filter_by(id = id).first()
+    if not user:
+        flash("Couldn't find user to promote", "danger")
+    else:
+        user.level = 2
+        db.session.commit()
+        flash(f'{user.username} was promoted to level: 2', "success")
+    return redirect(url_for('admin_users.users'))
