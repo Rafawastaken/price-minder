4d730a6089ef72324b8a04ee4a378c181dfc695f
Rafael Pimenta <jrafapimenta@gmail.com>
2023-07-15 01:57:55 +0100

    form to add game manually

---
+++ webapp/price_minder/routes/backend/games_management/forms.py
@@ -0,0 +1,24 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, DateField, BooleanField
+from wtforms.validators import DataRequired, Email, EqualTo
+
+
+# Sign-up forms
+class AddGameForm(FlaskForm):
+    # Meta
+    steam_id = StringField('Name', validators=[DataRequired()])
+    name = StringField('Name', validators=[DataRequired()])
+    release_date = StringField('Release Date', validators=[DataRequired()])
+    type_product = StringField('Product Type', validators=[DataRequired()])
+    description = StringField('Description', validators=[DataRequired()])
+    
+    # Images
+    link_header = StringField('link_header', validators=[DataRequired()])
+    capsule_image = StringField('capsule_image', validators=[DataRequired()])
+    capsule_imagev5 = StringField('capsule_imagev5', validators=[DataRequired()])
+
+    # Price
+    price = StringField('link_header', validators=[DataRequired()])
+    on_sale = BooleanField('on_sale')
+    discount_price = StringField('discount_price', validators=[DataRequired()])
+    discount_percent =  StringField('discount_percent', validators=[DataRequired()])
--- webapp/price_minder/routes/backend/games_management/routes.py
+++ webapp/price_minder/routes/backend/games_management/routes.py
@@ -1,6 +1,8 @@
-from flask import Blueprint, request, url_for, render_template
+from flask import Blueprint, request, url_for, render_template, flash, redirect
 from flask_login import login_required, current_user
+
 from .models import Game, Genre
+from .forms import AddGameForm
 
 ##################### * Blueprint * #####################
 games_management = Blueprint('games', __name__)
@@ -13,6 +15,13 @@ def check_permission():
     if current_user.level >= 2: return True
     return False
 
+# Form errors flash
+def flash_errors(errors):
+    print(errors)
+    for error in errors:
+        for item in errors[error]:
+            flash(f"{error.title()} - {item}", "danger")
+
 
 ##################### * Routes Games Management * #####################
 
@@ -20,12 +29,29 @@ def check_permission():
 @games_management.route('/')
 @login_required
 def games_management_index():
+    
+    if not check_permission():
+        flash("You don't have permissions to view this area", "danger")
+        return redirect(url_for('pages.index_pages'))
+
     title = "Game List"
     games = Game.query.all()
-    return render_template('./backend/games-management/games_list.html', games = games)
+    return render_template('./backend/games-management/games_list.html', title = title, games = games)
 
 
 @games_management.route('/add-game', methods = ['POST', 'GET'])
 @login_required
 def games_management_add():
-    ...
+    if not check_permission():
+        flash("You don't have permissions to view this area", "danger")
+        return redirect(url_for('pages.index_pages'))
+
+    title = "Add Games Forms"
+    form = AddGameForm()
+
+    if form.validate_on_submit():
+        ...
+    else:
+        flash_errors(form.errors)
+
+    return render_template('./backend/games-management/games_add.html', title = title, form = form)
